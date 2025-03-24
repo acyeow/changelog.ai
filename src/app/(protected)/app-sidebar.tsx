75a0a2a6835249a377f8eb912a6b25d687ec46df
acyeow <acyeow@cudavis.edu>
2025-03-24 12:03:29 -0700

    sidebar and placeholder pages for each route

---
+++ src/app/(protected)/app-sidebar.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { title } from "process";
+import { Button } from "~/components/ui/button";
+import { Plus } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "~/components/ui/sidebar";
+import { cn } from "~/lib/utils";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Q&A",
+    url: "/qa",
+    icon: Bot,
+  },
+  {
+    title: "Meetings",
+    url: "/meetings",
+    icon: Presentation,
+  },
+  {
+    title: "Billing",
+    url: "/billing",
+    icon: CreditCard,
+  },
+];
+
+const projects = [
+  {
+    name: "Project 1",
+  },
+  {
+    name: "Project 2",
+  },
+  {
+    name: "Project 3",
+  },
+];
+
+export function AppSidebar() {
+  const pathname = usePathname();
+  return (
+    <Sidebar collapsible="icon" variant="floating">
+      <SidebarHeader>Logo</SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => {
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        href={item.url}
+                        className={cn({
+                          "!bg-primary !text-white": pathname === item.url,
+                        })}
+                      >
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Projects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {projects.map((project) => {
+                return (
+                  <SidebarMenuItem key={project.name}>
+                    <SidebarMenuButton asChild>
+                      <div>
+                        <div
+                          className={cn(
+                            "text-primary flex size-6 items-center justify-center rounded-sm border bg-white text-sm",
+                            {
+                              "bg-primary text-white": false, // Add condition here to highlight the active project
+                            },
+                          )}
+                        >
+                          {project.name[0]}
+                        </div>
+                        <span>{project.name}</span>
+                      </div>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+              <div className="h-2"></div>
+              <SidebarMenuItem>
+                <Link href="/create">
+                  <Button size="sm" variant="outline" className="w-fit">
+                    <Plus />
+                    Create Project
+                  </Button>
+                </Link>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
